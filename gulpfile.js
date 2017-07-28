33f37fb49bce37563870b63d91af11a5516b4d1b
Jasper <jasper.jiang@pwc.com>
2017-07-28 15:47:12 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-var gulp = require('gulp')
-		$ = require("gulp-load-plugins")();		
+var gulp = require('gulp');
+		$ = require("gulp-load-plugins")();	
+		http = require("http");	
+		gutil=require('gulp-util')
+		ecstatic = require("ecstatic");
+		inject = require('gulp-inject')
 
 gulp.task('copy', function(){
 	return gulp
-					.src('app/index.html')
+					.src('src/index.html')
     			.pipe(gulp.dest('dist'));
 });
 
@@ -15,8 +19,8 @@ gulp.task('clean', function(){
 
 gulp.task('js', function(){
 	return gulp
-					.src('app/js/**/*.js',{
-						base: 'app'
+					.src('src/js/**/*.js',{
+						base: 'src'
 					})
 					.pipe($.uglify())
 					.pipe(gulp.dest('dist'));
@@ -24,15 +28,15 @@ gulp.task('js', function(){
 
 gulp.task('jshint', function(){
 	return gulp
-					.src('app/js/**/*.js')
+					.src('src/js/**/*.js')
 					.pipe($.jshint())
 					.pipe(jshint.reporter('default'));
 });
 
 gulp.task('htmlmin', function(){
 	return gulp
-					.src('app/**.html',{
-						base: 'app'
+					.src('src/**.html',{
+						base: 'src'
 					})
 					.pipe($.htmlmin())
 					.pipe(gulp.dest('dist'))
@@ -43,11 +47,25 @@ gulp.task('watch', function(){
 })
 
 gulp.task('usemin', function() {
-  return gulp.src('app/index.html')
+  return gulp.src('src/index.html')
     .pipe($.usemin({
       js: [$.uglify(),$.rev()],
       css: [$.minifyCss(),'concat']
     }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function(){ 
+		http.createServer(ecstatic({
+        root: 'dist',
+        cache: 0
+    })).listen(8080);
+    gutil.log(gutil.colors.blue('HTTP server listening on port 8080'));
+});
+
+gulp.task('index', function () {
+  return gulp.src('src/index.html')
+  	.pipe(inject(gulp.src(['src/**/*.js', 'src/**/*.css','bower_components/**/*.min.css','bower_components/**/*.min.js'], {read: false})))
     .pipe(gulp.dest('dist'));
 });
 
